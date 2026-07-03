c679b1517dc614e7fe546cd6d8160eaf44815460
Riya Jayaswal <riyajayaswal77@gmail.com>
2026-07-03 23:01:41 +0530

    Time: 0 ms (100%), Space: 43.1 MB (33.03%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -1,27 +1,27 @@
 class Solution {
     public String interpret(String command) {
-       int n=command.length();
-       StringBuilder ans=new StringBuilder();
-       int i=0;
-       while(i<n){
-        char ch=command.charAt(i);
-        if(ch=='G'){
-            ans.append("G");
-            i++;
-        }else if(ch=='('){
-            if(command.charAt(i+1)==')'){
-                ans.append("o");
-                i+=2;
+        int n=command.length();
+        StringBuilder ans=new StringBuilder();
+        int i=0;
+        while(i<n){
+            char ch=command.charAt(i);
+            if(ch=='G'){
+                ans.append("G");
+                i++;
+            }else if(ch=='('){
+                if(command.charAt(i+1)==')'){
+                    ans.append("o");
+                    i+=2;
+                }else{
+                    ans.append("a");
+                    ans.append("l");
+                    i+=3;
+                }
             }else{
-                ans.append("a");
-                ans.append("l");
-                i+=3;
+                i++;
             }
-        }else{
-            i++;
         }
-       }
-       return ans.toString();
+        return ans.toString();
     }
-     
+
 }
