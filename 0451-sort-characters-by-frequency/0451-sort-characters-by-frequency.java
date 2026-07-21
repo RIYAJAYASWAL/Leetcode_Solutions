287be909d9734fe33ac2267888c65a98258dcf20
Riya Jayaswal <riyajayaswal77@gmail.com>
2026-07-21 22:45:10 +0530

    Time: 24 ms (10.75%), Space: 46.3 MB (88.87%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,38 +1,35 @@
 class Solution {
     public String frequencySort(String s) {
-        int n = s.length();
-        StringBuilder ans = new StringBuilder();
-        int[] freq = new int[256];
+
+        int n=s.length();
+        StringBuilder ans=new StringBuilder();
+        int[] freq=new int[256];
         int[] first = new int[256];
+        Arrays.fill(first,-1);
 
-        Arrays.fill(first, -1);
-
-        for (int i = 0; i < n; i++) {
-            char x = s.charAt(i);
+        for(int i=0;i<n;i++){
+            char x=s.charAt(i);
             freq[x]++;
 
-            if (first[x] == -1) {
-                first[x] = i;
+            if(first[x]==-1) {
+                first[x]=i;
             }
         }
 
-        Character[] ch = new Character[n];
-
-        for (int i = 0; i < n; i++) {
-            ch[i] = s.charAt(i);
+        Character[] ch=new Character[n];
+        for(int i=0;i<n;i++){
+            ch[i]=s.charAt(i);
         }
-
-        Arrays.sort(ch, (a, b) -> {
-            if (freq[a] != freq[b]) {
-                return freq[b] - freq[a];
+        Arrays.sort(ch,(a,b)->{
+            if(freq[a] != freq[b]){
+                return freq[b]-freq[a];
             }
-            return first[a] - first[b];
+            return first[a]-first[b];
         });
 
-        for (char x : ch) {
+        for(char x:ch){
             ans.append(x);
         }
-
         return ans.toString();
     }
 }
