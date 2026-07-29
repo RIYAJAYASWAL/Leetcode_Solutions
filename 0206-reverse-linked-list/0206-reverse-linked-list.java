f37c56f5d63ae2d073ec926a0560556eee2ceeb3
Riya Jayaswal <riyajayaswal77@gmail.com>
2026-07-29 08:48:16 +0530

    Time: 0 ms (100%), Space: 44.3 MB (31.09%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -10,14 +10,13 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode prev=null;
-        ListNode curr=head;
-        while(curr!=null){
-            ListNode next=curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=next;
+        if(head==null||head.next==null){
+            return head;
         }
-        return prev;
+        ListNode newHead=reverseList(head.next);
+        head.next.next=head;
+        head.next=null;
+        return newHead;
     }
-}
+}//T.C->O(N)
+//S.C->O(1) BUT AUXILIARY S.C WILL BE O(N) AS IT IS STORING IN RECURSIVE STACK
