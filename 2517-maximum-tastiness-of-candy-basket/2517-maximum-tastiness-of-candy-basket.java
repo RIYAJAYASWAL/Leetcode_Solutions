ed0db3b072eb11ffc45ded867090cec538451930
Riya Jayaswal <riyajayaswal77@gmail.com>
2026-08-08 22:06:13 +0530

    Time: 53 ms (5.42%), Space: 65.7 MB (93.32%) - LeetHub

---
+++ 2517-maximum-tastiness-of-candy-basket/2517-maximum-tastiness-of-candy-basket.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maximumTastiness(int[] price, int k) {
+        Arrays.sort(price);
+        int n=price.length;
+        int e=price[n-1];
+        int s=0;
+        int ans=0;
+
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            int last=price[0];
+            int candy=1;
+
+            for(int i=1;i<n;i++){
+                if(Math.abs(price[i]-last)>=mid){
+                    last=price[i];
+                    candy++;
+                }
+            }
+            if(candy>=k){
+                ans=mid;
+                s=mid+1;
+            }else{
+                e=mid-1;
+            }
+        }
+        return ans;
+    }
+}
