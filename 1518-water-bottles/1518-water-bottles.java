b816267db6fba595a3ef6aeefd5cd97c4fc5ab87
Riya Jayaswal <riyajayaswal77@gmail.com>
2026-08-16 22:21:41 +0530

    Time: 0 ms (100%), Space: 41.9 MB (75.14%) - LeetHub

--- 1518-water-bottles/1518-water-bottles.java
+++ 1518-water-bottles/1518-water-bottles.java
@@ -1,12 +1,6 @@
 class Solution {
     public int numWaterBottles(int numBottles, int numExchange) {
 
-        int total=numBottles;
-
-        while(numBottles>=numExchange){
-            total+=numBottles/numExchange;
-            numBottles=(numBottles/numExchange)+(numBottles%numExchange);
-        }
-        return total;
+        return (numBottles*numExchange-1)/(numExchange-1);
     }
 }
