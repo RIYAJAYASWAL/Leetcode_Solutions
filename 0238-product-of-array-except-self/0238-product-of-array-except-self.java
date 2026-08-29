38ceae796682882757b905007861ca0831e3e490
Riya Jayaswal <riyajayaswal77@gmail.com>
2026-08-29 19:03:49 +0530

    Time: 2 ms (93.45%), Space: 65.1 MB (76.72%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -3,17 +3,23 @@ class Solution {
         
         int n=nums.length;
         int[] ans=new int[n];
+        int[] left=new int[n];
+        int[] right=new int[n];
         int prefix=1;
        //left side
         for(int i=0;i<n;i++){
-            ans[i]=prefix;
+            left[i]=prefix;
             prefix*=nums[i];
         }
         //right side;
         int suffix=1;
         for(int i= n-1;i>=0;i--){
-            ans[i]*=suffix;
+            right[i]=suffix;
             suffix*=nums[i];
+        }
+        //multiply both to get ans
+        for(int i=0;i<n;i++){
+            ans[i]=left[i]*right[i];
         }
         return ans;
     }
